73a23ba433ad76cdf4e77f271a883ff88c7ad461
lanzarot <luis-389@hotmail.com>
2022-09-22 10:27:21 -0500

    project with esbuiild

---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import App from './components/App'
+
+ReactDOM.render(
+    <App message="project ESBuild Example" />,
+    document.getElementById("app")
+)
